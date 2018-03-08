9e1a2091cb164a773b166b410d19853923a2234b
karagarvey <kgarvey87@gmail.com>
2018-03-08 20:28:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
   return "I can\'t hear you!"
 }
   else if (string === string.toUpperCase()) {
-    return "Yes, indeed!"
+    return "YES INDEED!"
   }
   else if (string === "I love you") {
     return "I love you, too"  }
